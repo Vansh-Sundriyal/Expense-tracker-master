7c6f76431d7a656141fdf91fc9f04e91d21c669b
Vansh <vanshsundriyal2004@gmail.com>
2026-06-07 16:12:26 +0530

    feat: implement expense CRUD API with JSON persistence

--- server/controllers/expenseController.js
+++ server/controllers/expenseController.js
@@ -31,7 +31,67 @@ const addExpense = (req, res) => {
   res.status(201).json(newExpense);
 };
 
+
+const updateExpense = (req, res) => {
+  const expenses = JSON.parse(
+    fs.readFileSync(filePath, "utf-8")
+  );
+
+  const { id } = req.params;
+
+  const index = expenses.findIndex(
+    expense => expense.id === id
+  );
+
+  if (index === -1) {
+    return res.status(404).json({
+      message: "Expense not found",
+    });
+  }
+
+  expenses[index] = {
+    ...expenses[index],
+    ...req.body,
+  };
+
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(expenses, null, 2)
+  );
+
+  res.json(expenses[index]);
+};
+
+const deleteExpense = (req, res) => {
+  const expenses = JSON.parse(
+    fs.readFileSync(filePath, "utf-8")
+  );
+
+  const { id } = req.params;
+
+  const filteredExpenses = expenses.filter(
+    expense => expense.id !== id
+  );
+
+  if (filteredExpenses.length === expenses.length) {
+    return res.status(404).json({
+      message: "Expense not found",
+    });
+  }
+
+  fs.writeFileSync(
+    filePath,
+    JSON.stringify(filteredExpenses, null, 2)
+  );
+
+  res.json({
+    message: "Expense deleted",
+  });
+};
+
 module.exports = {
   getExpenses,
   addExpense,
+  updateExpense,
+  deleteExpense,
 };
--- server/routes/expenseRoutes.js
+++ server/routes/expenseRoutes.js
@@ -3,6 +3,8 @@ const express = require("express");
 const {
   getExpenses,
   addExpense,
+  updateExpense,
+  deleteExpense,
 } = require("../controllers/expenseController");
 
 const router = express.Router();
@@ -10,5 +12,9 @@ const router = express.Router();
 router.get("/", getExpenses);
 
 router.post("/", addExpense);
+
+router.put("/:id", updateExpense);
+
+router.delete("/:id", deleteExpense);
 
 module.exports = router;
